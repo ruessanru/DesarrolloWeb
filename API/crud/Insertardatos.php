fb775fa058a50ccf03dbacf45c73f6608ec6baf0
ruessanru <rssrsanru@gmail.com>
2021-12-10 18:19:41 -0500

    otras modificaciones

--- API/crud/Insertardatos.php
+++ API/crud/Insertardatos.php
@@ -1,6 +1,9 @@
 <?php
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
+header('Access-Control-Allow-Methods: POST');
+
+if($_SERVER['REQUEST_METHOD'] === 'POST'){
 
 $json = file_get_contents('php://input');
 
@@ -21,6 +24,7 @@ $response -> mensaje = 'Se registró con éxito';
 
 header('Content-Type: application/json');
 
-
+echo json_encode($response);
+}
 
 ?>
